da92bfb7930bd3ee96933f77384bf2f57cd8c8a3
Norbi <norbyb26@yahoo.com>
2022-03-23 23:10:15 +0200

    added file upload logic

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-import imp
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+import os
+from os.path import join, dirname, realpath
+import pandas as pd
 
 app = Flask(__name__)
-api = Api(app)
 
-class Template(Resource):
-    def get(self, name):
-        return {"name": name}
+# enable debugging mode
+app.config["DEBUG"] = True
 
-api.add_resource(Template, "/hello/<string:name>")
+# Upload folder
+UPLOAD_FOLDER = 'static/files'
+app.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
 
-if __name__ == "__main__":
-    app.run(debug = True)
+# Get the uploaded files
+@app.route("/", methods=['POST'])
+def uploadFiles():
+      # get the uploaded file
+      uploaded_file = request.files['file']
+      if uploaded_file.filename != '':
+           file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+          # set the file path
+           uploaded_file.save(file_path)
+          # save the file
+          ####################
+          # True for now
+      return True
+
+if (__name__ == "__main__"):
+     app.run(port = 5000)
