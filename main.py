d0389098102a80a13310d4ecb55212a46b5b2bd1
Norbi <norbyb26@yahoo.com>
2022-03-24 13:38:01 +0200

    updated main

--- main.py
+++ main.py
@@ -13,18 +13,22 @@ UPLOAD_FOLDER = 'static/files'
 app.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
 
 # Get the uploaded files
-@app.route("/", methods=['POST'])
+@app.route("/<fileName>/<operation>", methods=['POST'])
 def uploadFiles():
-      # get the uploaded file
-      uploaded_file = request.files['file']
-      if uploaded_file.filename != '':
-           file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
-          # set the file path
-           uploaded_file.save(file_path)
-          # save the file
-          ####################
-          # True for now
-      return True
+    # get the uploaded file
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        # set the file path
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+        # save the file
+        uploaded_file.save(file_path)
+        ####################
+        #
+        # clustering script call
+        #
+        ####################
+        # True for now
+    return True
 
 if (__name__ == "__main__"):
      app.run(port = 5000)
